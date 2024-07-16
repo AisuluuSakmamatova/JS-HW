2b8d6d52a84f27ba83e3874b11e07448dfda7129
AisuluuSakmamatova <a_92kg@gmail.com>
2024-07-16 21:27:57 +0300

    finished hw-2

---
+++ script.js
@@ -0,0 +1,81 @@
+let firstName = "Nooruz"
+console.log(firstName);
+
+let secondUserName = "Zhakshylyk"
+console.log(secondUserName);
+
+let thirdUserName = "Alisher"
+console.log(thirdUserName);
+
+let fourthUserName = "Elizar"
+console.log(fourthUserName);
+
+const firstUserPoint = 97
+console.log(firstUserPoint);
+
+const secondUserPoint = 89
+console.log(secondUserPoint);
+
+const thirdUserPoint = 75
+console.log(thirdUserPoint);
+
+const fourthUserPoint = 90
+console.log(fourthUserPoint);
+
+const result1 = `1.${firstName}-${firstUserPoint} баллов из 100`
+console.log(result1);
+
+const result2 = `2.${secondUserName}-${secondUserPoint} баллов из 100`
+console.log(result2);
+
+const result3 = `3.${thirdUserName}-${thirdUserPoint} баллов из 100`
+console.log(result3);
+
+const result4 = `4.${fourthUserName}-${fourthUserPoint} баллов из 100`
+console.log(result4);
+
+const resultt = "Экзамендин орточо баллы 87.75 балл"
+console.log(resultt);
+
+let number = 4
+
+console.log(`1 x ${number} = ${ 1 * number}`);
+
+let number1 = 4
+
+console.log(`2 x ${number} = ${2 * number1}`);
+
+let number2 =4
+
+console.log(`3 x ${number} = ${3 * number2}`);
+
+let number3 = 5
+
+console.log(`4 x ${number} = ${4 * number3}`);
+
+let number4 = 6
+
+console.log(`5 x ${number} = ${6 * number4}`);
+
+let number5 = 7
+
+console.log(`7 x ${number} = ${7 * number5}`);
+
+let number6 = 8
+console.log(`8 x ${number} = ${8 * number6}`);
+
+let number7 = 9
+console.log(`9 x ${number} = ${ 9 * number7}`);
+
+let number8 = 10
+console.log(`10 x ${number} = ${10 * number8}`);
+
+
+let alerts = alert("вы успешно зарегистрировались");
+console.log(alerts);
+
+const confirms = confirm("При перезагруске сайта, введенные данные на форме будут очищены вы уверенны что хатите перезагрузить");
+console.log(confirms);
+
+let prompts = prompt("Введите ваше имя");
+console.log(prompts);
